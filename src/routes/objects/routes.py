fc2b5e3b74b298678d9c2ee8000d175f70e1fd87
doctashay <doctashay@github.com>
2023-08-20 10:33:24 -0600

    (feat)destroy_obj, load_obj, save_obj: ensure endpoints are working
    also ensures that nested item saving is working

--- src/routes/objects/routes.py
+++ src/routes/objects/routes.py
@@ -12,27 +12,59 @@ def save_obj():
     
     data = request.json
     items = data.get('items', [])
-    state = data.get('state', [])
     model = data.get('model', [])
+    state = data.get('state', {})
+    # Ensure 'pos', 'dir', and 'up' are present in state_vars
+    pos = data.get('pos')
+    dir = data.get('dir')
+    up = data.get('up')
+    if not pos or not dir or not up:
+        return jsonify({'status': 'error', 'message': 'Missing pos, dir, or up'}), 400
+    
+    # Extract 'items' and nested 'items' data properly
+    extracted_items = []
+    for item in items:
+        item_data = {
+            'slot': item.get('slot', ''),
+            'type': item.get('type', ''),
+            'state': item.get('state', {}),
+            'items': []  # Initialize empty nested items array
+        }
 
+        nested_items = item.get('items', [])  # Extract nested items
+        for nested_item in nested_items:
+            nested_item_data = {
+                'slot': nested_item.get('slot', ''),
+                'type': nested_item.get('type', ''),
+                'state': nested_item.get('state', {})
+            }
+            item_data['items'].append(nested_item_data)  # Append nested item
+
+        extracted_items.append(item_data)
+    
     document = {
         "oid": oid,
-        "items": items,
+        "items": extracted_items,  # Use extracted_items here
         "state": state,
         "model": model,
+        "pos": pos,
+        "dir": dir,
+        "up": up,
     }
-    
+
     # Check if document with given oid exists in the database
     existing_doc = Interfaces.objects.find_one({'oid': oid})
     if existing_doc:
         # Update the existing document with the new data
-        Interfaces.objects.update({'oid': oid}, {'$set': {'state': state}})
+        Interfaces.objects.update({'oid': oid}, {'$set': document})
     else:
         # Create a new document with the given oid and data
         Interfaces.objects.insert(document)
 
     log("/objects/save_obj", f"[{oid}] Saved object.")
     return jsonify({'status': 'success'}), 200
+
+
     
 @objects.route("/DayZServlet/objects/load_obj/", methods=["POST", "GET"])
 def load_obj():
@@ -45,19 +77,42 @@ def load_obj():
     if obj is None:
         return jsonify({'status': 'error', 'message': 'Object not found'}), 404
 
+    extracted_items = []
+    for item in obj.get('items', []):
+        item_data = {
+            'slot': item.get('slot', ''),
+            'type': item.get('type', ''),
+            'state': item.get('state', {})
+        }
+
+        nested_items = item.get('items', [])  # Extract nested items
+        nested_items_data = []  # Structure for nested items
+
+        for nested_item in nested_items:
+            nested_item_data = {
+                'slot': nested_item.get('slot', ''),
+                'type': nested_item.get('type', ''),
+                'state': nested_item.get('state', {})
+            }
+            nested_items_data.append(nested_item_data)
+
+        item_data['items'] = nested_items_data
+        extracted_items.append(item_data)
 
     response_data = {
         'model': obj['model'],
-        'items': obj['items'],
-        'state': obj['state']
+        'items': extracted_items,  # Use extracted_items here
+        'pos': obj.get('pos'),
+        'dir': obj.get('dir'),
+        'up': obj.get('up')
     }
 
-    log("/objects/load_obj/", f"[{obj}] [{oid}] served object.")
+    log("/objects/load_obj/", f"[{oid}] served object into world.")
     return jsonify(response_data)
 
-@objects.route("/DayZServlet/objects/kill_obj/", methods=["POST", "GET"])
-def kill_obj():
-    oid = request.args.get('obj', None, str)
+@objects.route("/DayZServlet/objects/destroy_obj/", methods=["POST", "GET"])
+def destroy_obj():
+    oid = request.args.get('oid', None, str)
     if not oid:
         return jsonify({'status': 'error', 'message': 'Missing object ID'}), 400
 
@@ -68,7 +123,7 @@ def kill_obj():
 
     query = Interfaces.objects.delete({'oid': oid})
     if query.deleted_count:
-        log("/objects/remove/", f"[{oid}] removed from world.")
+        log("/objects/destroy_obj/", f"[{oid}] removed from world.")
     else:
         # log("/world/remove/", f"[{item['type']}] does not exist in world.")
         pass
@@ -78,17 +133,17 @@ def kill_obj():
 
 @objects.route("/DayZServlet/objects/count_obj/", methods=["GET"])
 def count_obj():
-        # Fetch all documents from the database
-        all_documents = Interfaces.objects.find()
+    # Fetch all documents from the database
+    all_documents = Interfaces.objects.find()
 
-        # Initialize a list to store the OIDs
-        oids = []
+    # Initialize a list to store the OIDs
+    oids = []
 
-        # Collect OIDs from the documents
-        for doc in all_documents:
-            oid = doc.get("oid")
-            if oid:
-                oids.append(oid)
+    # Collect OIDs from the documents
+    for doc in all_documents:
+        oid = doc.get("oid")
+        if oid:
+            oids.append(oid)
 
-        # Return the list of OIDs as a JSON array
-        return jsonify({'count': oids}), 200
+    # Return the count of OIDs as well as the list of OIDs as a JSON response
+    return jsonify({'count': oids}), 200
